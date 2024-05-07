05f10fb3554c88953cf8a2bad70cab0fef2c16e3
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2024-05-07 17:48:51 +0700

    code logic logout on Header when click and edit button on Hero

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,10 @@
 "use client"
-import { useSession } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
 
 const Header = () => {
     const session = useSession();
-    // console.log(session);
+    console.log(session);
     const status = session.status;
     return (
         <header className='flex items-center justify-between'>
@@ -18,13 +18,16 @@ const Header = () => {
             <nav className='flex items-center gap-4 text-gray-500 font-semibold'>
                 {
                     status === 'authenticated' && (
-                        <button className='bg-primary rounded-full text-white px-8 py-2'>
+                        <button 
+                            onClick={() => signOut()}
+                            className='bg-primary rounded-full text-white px-8 py-2'
+                        >
                             Logout
                         </button>
                     )
                 }
                 {
-                    status !== 'authenticated' && (
+                    status === 'unauthenticated' && (
                         <>
                             <Link href={'/login'}>Login</Link>
                             <Link href={'/register'} className='bg-primary rounded-full text-white px-8 py-2'>
