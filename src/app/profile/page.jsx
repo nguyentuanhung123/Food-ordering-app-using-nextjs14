2db37b1b897297b52b6789cb0b21134c24e06051
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2024-05-08 17:43:11 +0700

    code logic loading when click button save on ProfilePage.jsx

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -9,6 +9,8 @@ const ProfilePage = () => {
     const session = useSession()
     // console.log(session);
     const [userName, setUserName] = useState('');
+    const [saved, setSaved] = useState(false);
+    const [isSaving, setIsSaving] = useState(false);
     const {status} = session;
 
     useEffect(() => {
@@ -19,11 +21,17 @@ const ProfilePage = () => {
 
     const handleProfileInfoUpdate = async (e) => {
         e.preventDefault();
+        setSaved(false)
+        setIsSaving(true)
         const response = await fetch('/api/profile', {
             method: 'PUT',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({name: userName})
         })
+        setIsSaving(false)
+        if(response.ok) {
+            setSaved(true)
+        }
     }
 
     if(status === 'loading') {
@@ -42,6 +50,20 @@ const ProfilePage = () => {
                 Profile
             </h1>
             <div className='max-w-md mx-auto'>
+                {
+                    saved && (
+                        <h2 className='text-center bg-green-100 p-4 rounded-lg border border-green-300'>
+                            Profile save!
+                        </h2>
+                    )
+                }
+                {
+                    isSaving && (
+                        <h2 className='text-center bg-blue-100 p-4 rounded-lg border border-blue-300'>
+                            Saving...
+                        </h2>
+                    )
+                }
                 <div className='flex gap-4 items-center'>
                     <div>
                         <div className='p-2 rounded-lg relative'>
